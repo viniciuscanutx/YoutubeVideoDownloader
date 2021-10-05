93c9e73995cd953102adc51f286f12227c67eaf0
viniciuscanutx <ofviniciuscanuto@gmail.com>
2021-10-05 16:06:49 -0300

    Implementando Recursos

--- Pythube.py
+++ Pythube.py
@@ -2,35 +2,57 @@
 from pytube import YouTube
 from time import sleep
 from pytube.cli import on_progress
+import moviepy.editor as mp
 
 while True:
 
+    print('----------------------------------------------------------------------')
     print('Olá, escolha o que deseja baixar usando os comandos abaixo.')
-
+    print('----------------------------------------------------------------------')
     print('1- Video\n2- Música')
+    print('----------------------------------------------------------------------')
     decis = input('Digite o que você deseja baixar:  ')
 
     if decis == '1':
         link = input('Cole o link do video do YouTube que deseja baixar: ')
         yt = YouTube(link, on_progress_callback=on_progress)
         title = yt.title
+        print('----------------------------------------------------------------------')
         print('Titulo =  ', title)
+        print('----------------------------------------------------------------------')
         print('Baixando...')
         video = YouTube(link)
         yt = yt.streams.get_highest_resolution()
         yt.download()
         sleep(1)
+        print('----------------------------------------------------------------------')
         print('Arquivo baixado.')
+        print('----------------------------------------------------------------------')
+        print('\n\n')
 
     elif decis == '2':
-        print('Baixando musica...')
+        link = input('Cole o link da Musica do YouTube que deseja baixar: ')
+        yt = YouTube(link, on_progress_callback=on_progress)
+        title = yt.title
+        print('----------------------------------------------------------------------')
+        print('Titulo =  ', title)
+        print('----------------------------------------------------------------------')
+        print('Baixando...')
+        video = YouTube(link)
+        yt = yt.streams.get_highest_resolution()
+        yt.download()
+        sleep(1)
+        print('----------------------------------------------------------------------')
+        print('Convertendo para mp3...')
+        print('----------------------------------------------------------------------')
 
     else:
         print('Opção Incorreta')
         sleep(2)
         break
 
-    print('Use 1 Para sim\nUse 2 Para não.')
+    print('Obs:\nUse 1 Para Sim\nUse 2 Para Não.')
+    print('----------------------------------------------------------------------')
     enter = input('Deseja baixar outro video ou musica?  ')
 
     if enter == '2':
