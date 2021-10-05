2e03b980f5da16a98f645d86c713e46e53d1ac4f
viniciuscanutx <ofviniciuscanuto@gmail.com>
2021-10-05 15:27:37 -0300

    Estrutura do projeto iniciada

--- Pythube.py
+++ Pythube.py
@@ -1,4 +1,37 @@
-#Importando Bibliotecas
+# Importando Bibliotecas
 from pytube import YouTube
 from time import sleep
 from pytube.cli import on_progress
+
+while True:
+
+    print('Olá, escolha o que deseja baixar usando os comandos abaixo.')
+
+    print('1- Video\n2- Música')
+    decis = input('Digite o que você deseja baixar:  ')
+
+    if decis == '1':
+        print('Baixando video...')
+
+    elif decis == '2':
+        print('Baixando musica...')
+
+    else:
+        print('Opção Incorreta')
+        sleep(2)
+        break
+
+    print('Use 1 Para sim\nUse 2 Para não.')
+    enter = input('Deseja baixar outro video ou musica?  ')
+
+    if enter == '2':
+        sleep(1)
+        break
+
+    elif enter == '1':
+        sleep(1)
+
+    else:
+        print('Opção Incorreta!')
+        sleep(2)
+        break
